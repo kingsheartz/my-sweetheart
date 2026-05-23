37ab0112788731e4387fdad5bf59629f32ecfb18
kingsheartz <govind.das279@gmail.com>
2026-05-23 21:36:30 +0530

    Initial commit: sweetheart's interactive page

---
+++ config.js
@@ -0,0 +1,44 @@
+const CONFIG = {
+    // --- index.html Settings ---
+    pageTitle: "Will you be my Valentine? 💕",
+    heading: "Will you be my Valentine? 💕",
+    yesButtonText: "Yes",
+    noButtonText: "No",
+    initialGif: "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",
+    musicSrc: "music/beabadoobee - Glue Song (Lyrics).mp3",
+    
+    // Interactive Arrays (Used when clicking 'No' or 'Yes')
+    gifStages: [
+        "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal
+        "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
+        "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
+        "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
+        "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
+        "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
+        "https://media.tenor.com/5_tv1HquZlcAAAAj/chiikawa.gif",               // 6 very devastated
+        "https://media1.tenor.com/m/uDugCXK4vI4AAAAC/chiikawa-hachiware.gif"  // 7 crying runaway
+    ],
+    noMessages: [
+        "No",
+        "Are you positive? 🤔",
+        "Pookie please... 🥺",
+        "If you say no, I will be really sad...",
+        "I will be very sad... 😢",
+        "Please??? 💔",
+        "Don't do this to me...",
+        "Last chance! 😭",
+        "You can't catch me anyway 😜"
+    ],
+    yesTeasePokes: [
+        "try saying no first... I bet you want to know what happens 😏",
+        "go on, hit no... just once 👀",
+        "you're missing out 😈",
+        "click no, I dare you 😏"
+    ],
+
+    // --- yes.html Settings ---
+    yesPageTitle: "Yay! 🎉",
+    yesHeading: "Knew you would say yes! 🎉",
+    yesGif: "https://media.tenor.com/eNHbizSfVb0AAAAj/lovemode-cute.gif",
+    yesMessage: "You just made me the happiest person! 💕"
+};
